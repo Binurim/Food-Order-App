8568d7450c4f5ceb5b2baf3b5b7842e7bde716da
binuri <binurim95@gmail.com>
2023-07-30 15:29:28 +0530

    Fixed indentation

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,22 +1,22 @@
 import CartContext from "./cart-context";
 
-const CartProvider = props => {
+const CartProvider = (props) => {
+  const addItemToCartHandler = (item) => {};
 
-    const addItemToCartHandler = item => {};
+  const removeItemFromCartHandler = (id) => {};
 
-    const removeItemFromCartHandler = id => {};
+  const cartContext = {
+    items: [],
+    totalAmount: 0,
+    addItem: addItemToCartHandler,
+    removeItem: removeItemFromCartHandler,
+  };
 
-    const cartContext = {
-        items: [],
-        totalAmount: 0,
-        addItem: addItemToCartHandler,
-        removeItem: removeItemFromCartHandler
-    };
-
-    return <CartContext.Provider value={cartContext}>
-        {props.children}
+  return (
+    <CartContext.Provider value={cartContext}>
+      {props.children}
     </CartContext.Provider>
-
+  );
 };
 
 export default CartProvider;
