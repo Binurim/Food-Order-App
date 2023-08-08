06b0eab2d530bada9101aeb864f92a11b592bd21
binuri <binurim95@gmail.com>
2023-08-08 23:04:10 +0530

    Fetched meals via http using firebase realtime DB

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,36 +1,37 @@
+import { useEffect, useState } from "react";
 import Card from "../UI/Card";
 import classes from "./AvailableMeals.module.css";
 import MealItem from "./MealItem/MealItem";
 
-const DUMMY_MEALS = [
-    {
-      id: 'm1',
-      name: 'Sushi',
-      description: 'Finest fish and veggies',
-      price: 22.99,
-    },
-    {
-      id: 'm2',
-      name: 'Schnitzel',
-      description: 'A german specialty!',
-      price: 16.5,
-    },
-    {
-      id: 'm3',
-      name: 'Barbecue Burger',
-      description: 'American, raw, meaty',
-      price: 12.99,
-    },
-    {
-      id: 'm4',
-      name: 'Green Bowl',
-      description: 'Healthy...and green...',
-      price: 18.99,
-    },
-  ];
-
 const AvailableMeals = () => {
-    const mealsList = DUMMY_MEALS.map((meal) => (
+
+  const [meals, setMeals] = useState([]);
+
+  useEffect(()=> {
+    //func you pass to useEffect should not return a promise --async
+    //May return a cleanup func which can be executed -- sync
+    const fetchMeals = async () => {
+      const response = await fetch('https://react-http-food-order-ap-38de1-default-rtdb.firebaseio.com/meals.json');
+      const responseData = await response.json();
+
+      // below config done due to firebase response data structure
+      const loadedMeals = [];
+
+      for (const key in responseData) {
+        loadedMeals.push({
+          id: key,
+          name: responseData[key].name,
+          description: responseData[key].description,
+          price: responseData[key].price,
+        })
+      };
+      setMeals(loadedMeals);
+    };
+
+    fetchMeals();
+  }, [])
+
+    const mealsList = meals.map((meal) => (
       <MealItem
         id = {meal.id}
         key={meal.id}
